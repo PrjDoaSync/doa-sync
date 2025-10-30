54959144bf77fb0492ec1172872406d840913942
vitorh07 <vitor07devsenai@gmail.com>
2025-10-30 19:38:17 -0300

    fix feat: Login #17

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea6e0ae6ed27d611ac4c3a87ceb4cc96419ce343")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7239bcd7054cf81bb759924f99a8570de52a21b")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
