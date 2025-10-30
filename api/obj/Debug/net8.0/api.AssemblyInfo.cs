fbdbdf0e935db130198626497f3418679ca1b2b9
Trincademes <vcavalciuk@gmail.com>
2025-10-30 19:21:00 -0300

    feat: Rota de Login #17

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c49962086049c98e2fcaf10ac4fcda926114fa4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea6e0ae6ed27d611ac4c3a87ceb4cc96419ce343")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
